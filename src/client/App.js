fdc16edb9f23faabb1ba59b4f17b18a60f687fa6
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-06-24 11:31:46 +0200

    fix issues with chakra ui

--- src/client/App.js
+++ src/client/App.js
@@ -5,22 +5,30 @@ import MealList from "./components/MealList";
 import Home from "./components/Home";
 import MealDetail from "./components/MealDetail";
 import './index.css'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 function App() {
 
   return (
-    <MealContextProvider >
-      <Router>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/meals">
-          <MealList />
-        </Route>
-        <Route exact path="/meal/:id">
-          <MealDetail />
-        </Route>
-      </Router>
-    </MealContextProvider>
+    <ChakraProvider>
+
+      <MealContextProvider >
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/meals">
+            <MealList />
+          </Route>
+          <Route exact path="/meal/:id">
+            <MealDetail />
+          </Route>
+        </Router>
+      </MealContextProvider>
+    </ChakraProvider>
+
   );
 }
 
